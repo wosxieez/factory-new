03eeadfb1cf7e3613c3914b8da26c1e25f9ca6b2
fc <1018555611@qq.com>
2020-07-06 16:29:18 +0800

    UI调整

--- src/view/department/AddFrom.jsx
+++ src/view/department/AddFrom.jsx
@@ -3,14 +3,14 @@ import { Modal, Form, Input } from 'antd'
 
 const AddFrom = Form.create({ name: 'from' })(props => {
   return (
-    <Modal {...props}>
-      <Form labelCol={{ span: 4 }} wrapperCol={{ span: 20 }}>
+    <Modal {...props} >
+      <Form labelCol={{ span: 4 }} wrapperCol={{ span: 18 }}>
         <Form.Item label='名称' hasFeedback>
           {props.form.getFieldDecorator('name', {
             rules: [{ required: true, message: '请输入名称' }]
-          })(<Input />)}
+          })(<Input placeholder='请输入名称' />)}
         </Form.Item>
-        <Form.Item label='备注'>{props.form.getFieldDecorator('remark')(<Input.TextArea rows={4} />)}</Form.Item>
+        <Form.Item label='备注'>{props.form.getFieldDecorator('remark')(<Input.TextArea rows={4} placeholder='选填' />)}</Form.Item>
       </Form>
     </Modal>
   )
--- src/view/department/View.jsx
+++ src/view/department/View.jsx
@@ -1,20 +1,22 @@
 import React, { useState, useRef } from 'react'
-import { Breadcrumb, Col, Row, Button, List } from 'antd'
+import { Breadcrumb, Col, Row, Button, List, Avatar, Icon } from 'antd'
 import AddFrom from './AddFrom'
 import api from '../../http'
-import { useEffect } from 'react'
-import { useCallback } from 'react'
+import { useEffect, useCallback } from 'react'
+
 
 export default () => {
   const [isAdding, setIsAdding] = useState(false)
   const addForm = useRef()
   const [dataSource, setDataSource] = useState([])
   const [departments, setDepartments] = useState([])
+  const [listIsLoading, setListIsLoading] = useState(false)
 
   const listData = useCallback(async () => {
     const response = await api.listDepartment(departments.length > 0 ? departments[departments.length - 1].id : null)
     if (response.code === 0) {
       setDataSource(response.data)
+      setListIsLoading(false)
     }
   }, [departments])
 
@@ -32,36 +34,44 @@ export default () => {
 
   useEffect(() => {
     listData()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [departments])
 
   return (
-    <div style={{ padding: 24 }}>
+    <div style={styles.root}>
+      <div style={styles.header}>
+        <div>
+          <Avatar style={styles.avatar} icon={<Icon type="apartment" />} />
+          <span style={styles.title}>中国节能</span>
+        </div>
+        <Button type='primary' icon={'plus'} onClick={setIsAdding.bind(this, true)}>添加部门</Button>
+      </div>
       <Row type='flex' align='middle'>
         <Col span={16}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>中国节能</Breadcrumb.Item>
-            {departments.map(department => (
-              <Breadcrumb.Item>{department.name}</Breadcrumb.Item>
+          <Breadcrumb style={styles.breadcrumb}>
+            <Breadcrumb.Item><a onClick={(e) => { setDepartments([]) }}>中国节能</a></Breadcrumb.Item>
+            {departments.map((department, index) => (
+              <Breadcrumb.Item key={index}><a onClick={(e) => { setDepartments(departments.slice(0, index + 1)) }}>{department.name}</a></Breadcrumb.Item>
             ))}
           </Breadcrumb>
         </Col>
-        <Col span={8}>
-          <Button style={{ float: 'right' }} onClick={setIsAdding.bind(this, true)}>
-            hello
-          </Button>
-        </Col>
       </Row>
       <List
+        loading={listIsLoading}
         dataSource={dataSource}
         renderItem={item => {
           return (
             <List.Item
+              style={styles.listItem}
               onClick={() => {
+                setListIsLoading(true)
                 const newDepartments = [...departments, item]
                 setDepartments(newDepartments)
               }}>
-              <List.Item.Meta title={item.name} description={item.remark} />
+
+              <List.Item.Meta
+                avatar={<Avatar style={styles.avatar} >{item.name}</Avatar>}
+                title={item.name} description={item.remark} />
             </List.Item>
           )
         }}
@@ -80,3 +90,28 @@ export default () => {
     </div>
   )
 }
+
+const styles = {
+  root: {
+    padding: '0 24px 0 24px'
+  },
+  title: {
+    marginLeft: 10,
+    fontSize: 15,
+  },
+  header: {
+    borderBottomStyle: 'solid', borderBottomWidth: 1, borderBottomColor: '#e8e8e8',
+    height: 50,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: 'space-between'
+  },
+  breadcrumb: {
+    marginTop: 8
+  },
+  listItem: {
+    cursor: 'pointer'
+  },
+  avatar: { backgroundColor: '#1890ff', verticalAlign: 'middle' }
+}
+
